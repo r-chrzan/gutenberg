0e53ba04c759bebb58eb109ce31c4a7109e6d0fb
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-10-14 19:27:31 +0300

    makes the navigation list view implementation a gutenberg experiment

--- packages/block-library/src/navigation/edit/index.js
+++ packages/block-library/src/navigation/edit/index.js
@@ -83,6 +83,9 @@ function Navigation( {
 	hasColorSettings = true,
 	customPlaceholder: CustomPlaceholder = null,
 } ) {
+	const isOffCanvasNavigationEditorEnabled =
+		window?.__experimentalEnableOffCanvasNavigationEditor === true;
+
 	const {
 		openSubmenusOnClick,
 		overlayMenu,
@@ -658,7 +661,12 @@ function Navigation( {
 							/* translators: %s: The name of a menu. */
 							actionLabel={ __( "Switch to '%s'" ) }
 						/>
-						<ListView blocks={ innerBlocks } isExpanded={ true } />
+						{ isOffCanvasNavigationEditorEnabled && (
+							<ListView
+								blocks={ innerBlocks }
+								isExpanded={ true }
+							/>
+						) }
 					</PanelBody>
 				</InspectorControls>
 				{ stylingInspectorControls }
@@ -832,7 +840,12 @@ function Navigation( {
 							/* translators: %s: The name of a menu. */
 							actionLabel={ __( "Switch to '%s'" ) }
 						/>
-						<ListView blocks={ innerBlocks } isExpanded={ true } />
+						{ isOffCanvasNavigationEditorEnabled && (
+							<ListView
+								blocks={ innerBlocks }
+								isExpanded={ true }
+							/>
+						) }
 					</PanelBody>
 				</InspectorControls>
 				{ stylingInspectorControls }
